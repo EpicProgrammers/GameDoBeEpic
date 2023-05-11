7a96a29585513076afad6e8d5a02363cf1acd1ae
Epicprogrammer69 <rytisdidziu@gmail.com>
2023-05-11 08:31:18 +0300

    Revert "Add files via upload"
    This reverts commit ae05cdc91047593f7a4ab237439fe5592ffd5004.

--- TheOnlyThingIKnowForReal/collisionTest.py
+++
@@ -1,6 +0,0 @@
-def collision_test(object_1, object_list):
-    hit_list = []
-    for obj in object_list:
-        if obj.colliderect(object_1):
-            hit_list.append(obj)
-    return hit_list
